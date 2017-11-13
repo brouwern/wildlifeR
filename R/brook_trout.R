98f4fadf13a499abfe4b2d4b457db54a475f8f1a
brouwern <brouwern@gmail.com>
2017-11-12 22:23:07 -0500

    misc updates

--- R/brook_trout.R
+++ R/brook_trout.R
@@ -1,6 +1,9 @@
 #' Brook trout impacts on Chinook salmon survival - original data
 #'
-#' ....
+#' Levin et al. 2002 studied the impacts of brook trout introduced for sport fishing
+#' on native Chinook salmon.  Whitlock & Shulter used a subset
+#' of these data as an example in their text Analysis of Biological Data
+#'
 #'
 #' @format A data frame, including:
 #' \describe{
@@ -10,9 +13,14 @@
 #'   \item{recap}{Number of juvenil chinook salmon "recapatured" by detecting their PIT tag; this was not reported in original paper but was estimated using published survival rate.  The survival rate estiamted using mark-recapture methods to account for detection probabilities.}
 #'   \item{surv}{survival rate}
 #' }
-#' @references Stoddard et al.  2017.  Avian egg shape: Form, function, and evolution.
-#' Science.  xxxx:xxx-xxx.
 #'
-#' @source \url{http://science.sciencemag.org/content/356/6344/1249}
+#'
+#' @source \url{http://rspb.royalsocietypublishing.org/content/269/1501/1663.short}
 
-"trout"
+#' @references Levin et al.  2002.  Non–indigenous brook trout and the demise of Pacific salmon:
+#' a forgotten threat?  PRSB 269.  DOI: 10.1098/rspb.2002.2063
+#'
+#' Whitlock & Schulter.  2nd ed.  Analysis of Biological Data.
+#'
+#'
+"brook_trout"
