10d4801be1d83ede6126127349d829bdf78b1bdf
brouwern <brouwern@gmail.com>
2017-12-04 01:22:09 -0500

    update

--- R/brook_trout.R
+++ R/brook_trout.R
@@ -17,7 +17,7 @@
 #'
 #' @source \url{http://rspb.royalsocietypublishing.org/content/269/1501/1663.short}
 
-#' @references Levin et al.  2002.  Non–indigenous brook trout and the demise of Pacific salmon:
+#' @references Levin et al.  2002.  Non-indigenous brook trout and the demise of Pacific salmon:
 #' a forgotten threat?  PRSB 269.  DOI: 10.1098/rspb.2002.2063
 #'
 #' Whitlock & Schulter.  2nd ed.  Analysis of Biological Data.
