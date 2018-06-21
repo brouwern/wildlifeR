268b78785c5f9c84c0b13c4b200a85433d06f5fc
brouwern <brouwern@gmail.com>
2018-06-20 23:29:48 -0400

    rebuild pkgdown site

--- R/human_heartrate.R
+++ R/human_heartrate.R
@@ -1,4 +1,7 @@
-#' Human heart rate data from American undergraduates
+#' Human body temperature data and heartrate
+#'
+#' Physiology data originally from Mackowiak et al (1992).  Developed as a demonstration for
+#' t-tests by Shoemaker (1996) and also used by Motulsky (2014).
 #'
 #' @format A data frame with 2 columns:
 #' \describe{
@@ -6,11 +9,19 @@
 #'   \item{sex}{sex of individual}
 #'   \item{heartrate}{heartrate in beats per minute}
 #' }
+#' @references Mackowiak et al.  1992.  A Critical Appraisal of 98.6°F, the Upper Limit of the Normal Body Temperature, and Other Legacies of Carl Reinhold August Wunderlich.  JAMA.  268:1578-1580.  https://jamanetwork.com/journals/jama/article-abstract/400116
+#' @references Motulsky, H.  2014.  Intuitive biostatistics: a nonmathematical guide to statistical thinking.
+#' @references Shoemaker, A.  1996.  What's Normal? -- Temperature, Gender, and Heart Rate.  Journal of Statistics Education v.4, n.2.  ww2.amstat.org/publications/jse/v4n2/datasets.shoemaker.html
+#'
+#'
+#' @source \url{https://www.fws.gov/midwest/eagle/population/nos_state_tbl.html}
+#' @source \url{http://www.media.pa.gov/pages/Game-Commission-Details.aspx?newsid=44}
+#'
 #'
 #' @examples
 #'
-#' ## Plot number of eagles over time
-#' plot(eagles ~ year, data = eaglesPA, col = 2)
-#'
+#' ## Histogram of body temperature
+#' hist(human_bodytemp$temp)
 
-"human_heartrate"
+
+"human_bodytemp"
